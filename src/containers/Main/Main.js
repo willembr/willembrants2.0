bdd285c48512521469fe4283a0c88bdf27d62e84
willembr <willembrants@hotmail.com>
2021-02-23 15:37:29 +0100

    Add routes for webpages

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,12 +1,22 @@
 import React,{Component} from 'react';
 import Navigation from '../Navigation/Navigation';
+import { Route, Switch } from 'react-router-dom';
+import Skills from '../Pages/skills/Skills';
+import Creations from '../Pages/creations/Creations';
+import Passions from '../Pages/passions/Passions';
+import Contact from '../Pages/contact/Contact';
  
 class Main extends Component{
     render(){
-
         return(
         <div>
-            <Navigation/>
+            <Switch>
+                <Route path='/' exact component={Navigation}/>
+                <Route path='/skills' component={Skills}/>
+                <Route path='/creations' component={Creations}/>
+                <Route path='/passions' component={Passions}/>
+                <Route path='/contact' component={Contact}/>
+            </Switch>
         </div>
         );
     }
