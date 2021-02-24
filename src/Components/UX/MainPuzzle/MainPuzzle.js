1aa8909798510db08740be0541c6ed1f6fdf2579
willembr <willembrants@hotmail.com>
2021-02-24 09:49:14 +0100

    Last cleanup MainPuzlle

--- src/Components/UX/MainPuzzle/MainPuzzle.js
+++ src/Components/UX/MainPuzzle/MainPuzzle.js
@@ -6,9 +6,7 @@ const MainPuzzle = (props) => {
     return(
         <div className="MainPuzzle" onDrop={(e) => props.dropped(e)} onDragOver={(e) => props.dragOver(e)} >   
             <div className="PuzzlePiece" onClick={props.toggle}>
-                <svg className="PuzzleSvg" x="0px" y="0px" viewBox="0 0 300 300">
                     { !props.fullPuzzle ? <FullPuzzle/> : <DottedPuzzle/> }
-                </svg>
             </div>    
             <h4 className="Menu">Menu</h4>     
         </div>
